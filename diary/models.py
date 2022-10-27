ef4dfc1960864058eb9dfc6c8d5c203340817767
kosclid <kos-2000@hanmail.net>
2022-10-27 16:51:07 +0900

    second mission

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Memory(models.Model):
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}] : {self.name}'
+
