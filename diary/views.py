8a2df4d931998a2076fe166fc964598286282508
kosclid <kos-2000@hanmail.net>
2022-10-27 17:00:05 +0900

    fourth mission

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from diary.models import Memory
 
-# Create your views here.
+def index(request):
+    # 전체 포스팅을 가져올 준비(가져오기 이전임)
+    dia_qs = Memory.objects.all().order_by('-pk')
+    return render(
+        request, 
+        "diary/memory_list.html",
+        {'dia_list' : dia_qs,
+        }
+        )
